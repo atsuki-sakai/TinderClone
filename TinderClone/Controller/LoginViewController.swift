f5679f1f4d06e85bf070fe316662c523ccc52d32
sakaiatuki <atk721@icloud.com>
2020-03-23 19:09:06 +0900

    loginPageとmainpageを作成　by Sakai

---
+++ TinderClone/Controller/LoginViewController.swift
@@ -0,0 +1,30 @@
+//
+//  LoginViewController.swift
+//  TinderClone
+//
+//  Created by 酒井専冴 on 2020/03/23.
+//  Copyright © 2020 sakai_atsuki. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
